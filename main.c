e73ec73803196c3c795871905250ae18121c2546
ViacheslavTrofymenko <trofymenko.v.n@gmail.com>
2025-06-13 07:16:02 +0200

    wip

--- main.c
+++ main.c
@@ -6,25 +6,28 @@
 /*   By: vtrofyme <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 00:59:35 by vtrofyme          #+#    #+#             */
-/*   Updated: 2025/06/10 01:40:06 by vtrofyme         ###   ########.fr       */
+/*   Updated: 2025/06/12 15:12:49 by vtrofyme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "push_swap.h"
 
+int	ft_check_arguments(char **argv)
+{
+
+}
+
 int	main(int argc, char **argv)
 {
-	t_stack	**stack_a;
-	t_stack	**stack_b;
+	t_stack	*a;
+	t_stack	*b;
 
-	if (argc < 2)
-		return (ft_error());
-	ft_check_args(argc, argv);
-	stack_a = ft_calloc(1, sizeof(t_stack));
-	if (!stack_a)
-		return (NULL);
-	stack_b = ft_calloc(1, sizeof(t_stack));
-	if (!stack_b)
-		return (NULL);
-	return (0);
+	a = NULL;
+	b = NULL;
+	if (argc == 1 || (argc == 2 && !argv[1][0]))
+		return (1);
+	else if (argc == 2)
+		argv = ft_split(argv[1], ' ');
+	ft_check_arguments();
+
 }
